OCA/module-composition-analysis

Add 'odoo_repository'
Module to build and maintain an exhaustive list of Odoo modules
and their metadata:

- dependencies between them
- license
- authors
- maintainers
- lines of code
- ...

odoo_repository/__manifest__.py
@@ -1,0 +1,50 @@
+# Copyright 2023 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    "name": "Odoo Repository Data",
+    "summary": "Base module to host data collected from Odoo repositories.",
+    "version": "16.0.1.0.0",
+    "category": "Tools",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/TODO",
+    "data": [
+        "security/ir.model.access.csv",
+        "data/ir_config_parameter.xml",
+        "data/ir_cron.xml",
+        "data/odoo_repository_org.xml",
+        "data/odoo_repository_addons_path.xml",
+        "data/odoo_repository.xml",
+        "data/odoo.repository.csv",
+        "data/queue_job.xml",
+        "views/menu.xml",
+        "views/odoo_author.xml",
+        "views/odoo_branch.xml",
+        "views/odoo_license.xml",
+        "views/odoo_maintainer.xml",
+        "views/odoo_module.xml",
+        "views/odoo_module_branch.xml",
+        "views/odoo_module_category.xml",
+        "views/odoo_module_dev_status.xml",
+        "views/odoo_python_dependency.xml",
+        "views/odoo_repository.xml",
+        "views/odoo_repository_addons_path.xml",
+        "views/odoo_repository_branch.xml",
+        "views/odoo_repository_org.xml",
+    ],
+    "installable": True,
+    "depends": [
+        # core
+        "base_sparse_field",
+        # OCA/queue
+        "queue_job",
+    ],
+    "external_dependencies": {
+        "python": [
+            "gitpython",
+            "odoo-addons-analyzer",
+            # TODO to publish
+            # "odoo-repository-scanner"
+        ],
+    },
+    "license": "AGPL-3",
+}

ForgeFlow/odoo-repository

odoo_repository/__manifest__.py
@@ -1,0 +1,52 @@
+# Copyright 2023 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    "name": "Odoo Repositories Data",
+    "summary": "Base module to host data collected from Odoo repositories.",
+    "version": "16.0.1.0.0",
+    "category": "Tools",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/TODO",
+    "data": [
+        "security/ir.model.access.csv",
+        "data/ir_config_parameter.xml",
+        "data/ir_cron.xml",
+        "data/odoo_repository_org.xml",
+        "data/odoo_repository_addons_path.xml",
+        "data/odoo_repository.xml",
+        "data/odoo.repository.csv",
+        "data/queue_job.xml",
+        "views/menu.xml",
+        "views/ssh_key.xml",
+        "views/odoo_author.xml",
+        "views/odoo_branch.xml",
+        "views/odoo_license.xml",
+        "views/odoo_maintainer.xml",
+        "views/odoo_module.xml",
+        "views/odoo_module_branch.xml",
+        "views/odoo_module_category.xml",
+        "views/odoo_module_dev_status.xml",
+        "views/odoo_python_dependency.xml",
+        "views/odoo_repository.xml",
+        "views/odoo_repository_addons_path.xml",
+        "views/odoo_repository_branch.xml",
+        "views/odoo_repository_org.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "depends": [
+        # core
+        "base_sparse_field",
+        # OCA/queue
+        "queue_job",
+    ],
+    "external_dependencies": {
+        "python": [
+            "gitpython",
+            "odoo-addons-analyzer",
+            # TODO to publish
+            # "odoo-repository-scanner"
+        ],
+    },
+    "license": "AGPL-3",
+}